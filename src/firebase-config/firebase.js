a1874f4bc1e5864f9fdc0a84da825e17cf34a264
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-08-22 12:53:34 +0400

    some changes

---
+++ src/firebase-config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFOu-JAEkTRmng5aNbX9x5LLzzmxzBCfE",
+    authDomain: "chat-app-d4733.firebaseapp.com",
+    projectId: "chat-app-d4733",
+    storageBucket: "chat-app-d4733.appspot.com",
+    messagingSenderId: "546873179616",
+    appId: "1:546873179616:web:70e345a6150bf37c27bf85",
+    measurementId: "G-42H0DX4P9S"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
