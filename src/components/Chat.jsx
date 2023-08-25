30958914c2f1971e0f895b3583b6c514436661fa
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-08-25 21:38:06 +0400

    make chat functional

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,28 +6,28 @@ import {
   serverTimestamp,
   onSnapshot,
   query,
-  where
+  where,
 } from "firebase/firestore";
 import { auth, db } from "../firebase-config/firebase";
 
 function Chat({ room }) {
   const [newMessage, setNewMessage] = useState("");
 
-  const [messages,setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
 
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
-    const queryMessages = query(messagesRef,where("room","==",room));
-    onSnapshot(queryMessages,(snapshot) => {
-        let messages = []
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
 
-        snapshot.forEach((doc) => {
-            messages.push({...doc.data(),id:doc.id})
-        })
-        setMessages(messages);
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
     });
-
+    return () => unsuscribe();
   }, []);
 
   const handleSubmit = async (e) => {
@@ -45,7 +45,20 @@ function Chat({ room }) {
 
   return (
     <div className="chat-app">
-        <div>{messages.map(message => <h1>{message.text}</h1>)}</div>
+      <div className="header">
+        <h1>Welcome to :{room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        <div>
+          {messages.map((message) => (
+            <div className="message" key={message.id}>
+              <span className="user">{message.user}</span>
+              {message.text}
+            </div>
+          ))}
+        </div>
+      </div>
+
       <form className="new-message-form" onSubmit={handleSubmit}>
         <input
           className="new-message-input"
@@ -54,7 +67,6 @@ function Chat({ room }) {
           value={newMessage}
         />
         <button type="submit" className="send-button">
-          {" "}
           Send
         </button>
       </form>
