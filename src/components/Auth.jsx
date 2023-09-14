03e8bdf9b631f93d2e0519072a5f8f9ff94c149a
ib0222 <ibrahim.ismayilzade221@gmail.com>
2023-09-14 22:11:01 +0400

    makin sign in button better

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,31 +1,27 @@
-  import {auth,provider} from '../firebase-config/firebase'
-  import {signInWithPopup} from 'firebase/auth'
+import { auth, provider } from "../firebase-config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import GoogleIcon from "@mui/icons-material/Google";
+import Cookies from "universal-cookie";
 
-  import  Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
-  const cookies = new Cookies()
-
-  export const Auth = ({setIsAuth}) => {
-
-
-      const signInWithGoogle = async () => {
-        try{
-          const result = await signInWithPopup(auth,provider);
-          cookies.set("auth-token",result.user.refreshToken);
-          setIsAuth(true)
-        }
-          catch(error){
-            console.log(error.message)
-          }
-      }
-
-    return (
-
-
-
-      <div className="auth">
-        <p>Sign in in with google</p>
-        <button onClick={signInWithGoogle}>Sign in with google</button>
-      </div>
-    );
+export const Auth = ({ setIsAuth }) => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
+
+  return (
+    <div className="auth">
+      <button onClick={signInWithGoogle}>
+        <GoogleIcon fontSize="large"/>
+        Sign in in with google
+      </button>
+    </div>
+  );
+};
